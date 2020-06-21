aa42f461b74a2a715959d0ffe5e7f5035baff718
Idalis Paz <idalispaz@gmail.com>
2020-06-21 02:02:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,8 +19,8 @@ def speak_to_grandma (phrase)
   elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
     
-  elsif phrase == "Hi!".upcase
-    return "HUH?! SPEAK UP, SONNY"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
     
   elsif phrase == "WHAT DID YOU DO TODAY?"
     return "NO, NOT SINCE 1938!"
